6db46a40ed535c5a82b21f0c52e6ed4c1b6e292f
Vladimir Danilov <dev.danilovl@gmail.com>
2025-04-19 09:05:14 +0200

    DoctrineEntityDtoExtension - change to DependencyInjection\Extension\Extension.

--- src/DependencyInjection/Extension/DoctrineEntityDtoExtension.php
+++ src/DependencyInjection/Extension/DoctrineEntityDtoExtension.php
@@ -6,7 +6,7 @@ use Danilovl\DoctrineEntityDtoBundle\Service\ConfigurationService;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class DoctrineEntityDtoExtension extends Extension
 {
